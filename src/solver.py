caf936ab2db82d962ad315d6971585ad0f428e02
Sanjeet N. Dasharath <sanjeetdasharath@gmail.com>
2021-08-07 20:25:58 +0530

    null space

--- src/solver.py
+++ src/solver.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Callable
 
 
 class Solver:
@@ -9,25 +10,58 @@ class Solver:
         self.cols = A.shape[1]
         self.pivots: list[tuple[int, int]] = []
 
-    def solve(self):
+    def solve(self) -> tuple[np.ndarray, list[np.ndarray]]:
         # create the augmented matrix from `A` and `b`
         augmatrix = self._augment()
-        print(augmatrix)
-        print("=============================")
         # get the row echelon form of the agumented matrix
         self._row_echelon_form(augmatrix)
-        print(augmatrix)
-        print("=============================")
         # get the reduced row echelon form
         self._reduced_row_echelon_form(augmatrix)
-        print(augmatrix)
         # get a particular solution
         bp = self._particular_sol(augmatrix)
-        print(self.A @ bp)
-        print("pivots:", self.pivots)
+        # get the null space of the matrix
+        null_space = self._null_space(augmatrix)
 
-    def _particular_sol(self, augmatrix: np.ndarray):
-        pivot_cols = [col for _, col in self.pivots]
+        return (bp, null_space)
+
+    def _null_space(self, augmatrix: np.ndarray) -> list[np.ndarray]:
+        sols: list[np.ndarray] = []
+        nsols = 0
+        pivot_cols = self._get_pivot_cols(set)
+
+        # last pivot column
+        col = 0
+        for row in range(self.rows):
+            # find next pivot column
+            for j in range(col, self.cols):
+                if j in pivot_cols:
+                    # set last pivot column to j
+                    col = j
+                    break
+            # initialize solution idx to 0
+            sol_idx = 0
+            # iterate through remaining columns to right
+            for j in range(col + 1, self.cols):
+                if j not in pivot_cols:
+                    # add solution vector if it does not exist
+                    if nsols <= sol_idx:
+                        sols.append(np.zeros((self.cols,)))
+                        nsols += 1
+                    # if j is a non pivot column, modify solution such
+                    # that (row, j) and (row, col) will cancel each other
+                    sols[sol_idx][j] = -1.0
+                    sols[sol_idx][col] = augmatrix[row, j]
+                    # increment solution idx
+                    sol_idx += 1
+
+            # increment column by one for next iteration since it's been used
+            col += 1
+
+        return sols
+
+    def _particular_sol(self, augmatrix: np.ndarray) -> np.ndarray:
+        # get list of pivot columns for indexing
+        pivot_cols = self._get_pivot_cols(list)
 
         bp = np.zeros((self.cols,))
         bp[pivot_cols] = augmatrix[:, -1]
@@ -44,6 +78,8 @@ class Solver:
     def _row_echelon_form(self, augmatrix: np.ndarray):
         row = 0
         for col in range(self.cols):
+            self._swap_first_nonzero(augmatrix, row, col)
+
             if (x := augmatrix[row, col]) != 0:
                 augmatrix[row, :] /= x
                 self._reduce_below(augmatrix, row, col)
@@ -61,8 +97,20 @@ class Solver:
             augmatrix[row, :] -= augmatrix[row, col] * vals
             row += 1
 
+    def _swap_first_nonzero(self, augmatrix: np.ndarray, row: int, col: int):
+        if augmatrix[row, col] != 0:
+            return
+
+        for i in range(row + 1, self.rows):
+            if augmatrix[i, col] != 0:
+                augmatrix[[row, i]] = augmatrix[[i, row]]
+                break
+
     def _augment(self) -> np.ndarray:
         return np.c_[self.A, self.b]
+
+    def _get_pivot_cols(self, container: Callable) -> list[int]:
+        return [col for _, col in self.pivots]
 
 
 if __name__ == "__main__":
@@ -75,4 +123,6 @@ if __name__ == "__main__":
 
     b = np.array([-3, 2, 0, 1])
     solver = Solver(A, b)
-    solver.solve()
+    bp, null_space = solver.solve()
+    print("Particular solution:", bp)
+    print("Null space:", null_space)
